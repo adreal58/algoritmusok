554c7f792df07b35a33606438f1fe40a954eacfc
adreal58 <161327125+adreal58@users.noreply.github.com>
2024-03-18 15:20:07 +0100

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int A[10]={ 13,28,43,37,10,5,8,99,101,2 };
+    int B[5]={6,43,37,10,88};
+    int i, j, cdb = 0, C[15];
+        
+    for ( i = 0; i < 10; i++)
+    {
+        cout << A[i] << " ";
+    }for ( j = 0; j < 5; j++)
+    {
+        cout << B[j] << " ";
+    }
+    cout << endl << endl;
+
+    
+   
+}
+
+
